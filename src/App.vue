9b9e7714a60c788428e773e736f881b52e886b27
DANIELNAHUN <nahuncf5@gmail.com>
2022-06-13 20:43:55 -0500

    test vuetify

--- src/App.vue
+++ src/App.vue
@@ -2,30 +2,11 @@
   <v-app>
     <v-app-bar
       app
-      color="primary"
+      color="purple"
       dark
+      clipped-left
     >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
+    <h3 class="text-center">MI PRIMER PROYECTO</h3>
 
       <v-btn
         href="https://github.com/vuetifyjs/vuetify/releases/latest"
